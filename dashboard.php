2aaa3804911266007d5e7e2ae04dd0e7ac58a3cc
Afreed Bin Haque <84925512+afreed-bh@users.noreply.github.com>
2021-05-29 13:52:14 +0600

    Update on dashboard
    Dashboard has been updated for primary view

--- dashboard.php
+++ dashboard.php
@@ -2,13 +2,19 @@
 session_start();
 if(isset($_SESSION['id']) && isset($_SESSION['name']) && isset($_SESSION['usr_email']) && isset($_SESSION['role']) && isset($_SESSION['status'])){
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'components/head.php';?>
+<?php include 'components/navbar_dashboard.php';?>
+<body>
 <!--------------------
 User or client dashboard here
 --------------------->
-<?php include 'components/head.php';?>
-<h1> Hi <?php echo $_SESSION['name'];?></h1>
-<?php
-else{
+<section class="main" id="page_search">
+</section>
+</body>
+</html>
+<?php }else{
   header("Location: login.php");
   exit();
 }
